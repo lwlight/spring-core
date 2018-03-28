a04569da3618a0fa24ce3a6f4c22a64671b4ef0d
Artsim Kniha <artsiom_kniha@epam.com>
2018-03-28 15:50:15 +0300

    begin of 5

--- src/main/java/com/epam/lab/spring/core/work/FileEventLogger.java
+++ src/main/java/com/epam/lab/spring/core/work/FileEventLogger.java
@@ -1,7 +1,35 @@
 package com.epam.lab.spring.core.work;
 
-public class FileEventLogger implements EventLogger {
-    public void logEvent(Event event) {
+import org.apache.commons.io.FileUtils;
 
+import java.io.File;
+import java.io.IOException;
+
+public class FileEventLogger implements EventLogger {
+    private String fileName;
+    private File file;
+
+    public FileEventLogger(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public void logEvent(Event event){
+        try {
+            FileUtils.writeStringToFile(file, fileName, true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private  void init() throws IOException{
+        this.file = new File(fileName);
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
     }
 }
